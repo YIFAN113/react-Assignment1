b423f1d18f34c47755642cebb072b303ac5233fe
Yifan Gu <20095253@mail.wit.ie>
2023-11-11 22:26:08 +0800

    add TopRatedMoviesPage

--- movies/src/index.js
+++ movies/src/index.js
@@ -11,6 +11,7 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
+import TopRatedMoviesPage from "./pages/topRatedMoviesPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -36,6 +37,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
           <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+          <Route path="/movies/top_Rated" element={ <TopRatedMoviesPage/>} />
         </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
