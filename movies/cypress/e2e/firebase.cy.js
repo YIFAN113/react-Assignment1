f6a36bbff11450aaf16aa083405f4926de10ca09
Yifan Gu <20095253@mail.wit.ie>
2023-11-16 22:45:01 +0800

    Retest the loginPage

--- movies/cypress/e2e/firebase.cy.js
+++ movies/cypress/e2e/firebase.cy.js
@@ -4,8 +4,8 @@ describe('LoginPage', () => {
     });
   
     it('allows a user to sign in', () => {
-      cy.get('input[type="email"]').type('20095253@mail.wit.ie');
-      cy.get('input[type="password"]').type('ZQEHQU9j2');
+      cy.get('input[name="email"]').type('20095253@mail.wit.ie');
+      cy.get('input[name="password"]').type('ZQEHQU9j2');
       cy.get('form').contains('Login').click();
       cy.url().should('eq', 'http://localhost:3000/')
     });
@@ -13,8 +13,8 @@ describe('LoginPage', () => {
     it('allows a user to sign up', () => {
       // avoid conflict with existing users
       const uniqueEmail = `test_user_${new Date().getTime()}@example.com`;
-      cy.get('input[type="email"]').type(uniqueEmail);
-      cy.get('input[type="password"]').type('password');
+      cy.get('input[name="email"]').type(uniqueEmail);
+      cy.get('input[name="password"]').type('password');
       cy.get('button').contains('Sign Up').click();
       cy.url().should('eq', 'http://localhost:3000/')
     });
