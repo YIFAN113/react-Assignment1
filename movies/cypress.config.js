5def0092bd201d59ffdcf829dde5a70571503ea3
Yifan Gu <20095253@mail.wit.ie>
2023-11-20 19:20:32 +0800

    fix cypress

--- movies/cypress.config.js
+++ movies/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
 
   e2e: {
-    "baseUrl": "http://localhost:3000/1",
+    "baseUrl": "http://localhost:3000/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
