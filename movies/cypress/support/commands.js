27fbe263561401bff6f1587fedb2591d8e0cd3da
Yifan Gu <20095253@mail.wit.ie>
2023-11-23 00:39:53 +0800

    add custom commands

--- movies/cypress/support/commands.js
+++ movies/cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('input[id=email]').type(email);
+    cy.get('input[id=password]').type(password);
+    // if use get(button), it has the same name as the header button.
+    cy.get('form').contains('Login').click();
+  });
+
+  
+  Cypress.Commands.add('register', (uniqueEmail, password) => {
+    cy.get('input[id=email]').type(uniqueEmail);
+    cy.get('input[id=password]').type(password);
+    cy.get('button').contains('Sign Up').click();
+  });
